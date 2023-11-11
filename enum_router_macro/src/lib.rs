544b2d514ab335b26192cb03a11728d40baa1fe7
Sean Walker <swlkr.rbl@gmail.com>
2023-11-10 19:36:18 -0700

    Ghetto 404

--- enum_router_macro/src/lib.rs
+++ enum_router_macro/src/lib.rs
@@ -45,6 +45,17 @@ fn routes_macro(input: DeriveInput) -> Result<TokenStream2> {
         .map(|(ident, uri)| quote! { #uri => #enum_name::#ident })
         .collect::<Vec<_>>();
 
+    let not_found_str_to_route = parts
+        .iter()
+        .filter(|(_, uri)| uri.value().ends_with("404"))
+        .map(|(ident, _)| quote! { _ => #enum_name::#ident })
+        .last();
+
+    let not_found = match not_found_str_to_route {
+        Some(not_found) => not_found,
+        None => quote! { _ => unimplemented!() },
+    };
+
     Ok(quote! {
         impl std::fmt::Display for #enum_name {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -58,7 +69,7 @@ fn routes_macro(input: DeriveInput) -> Result<TokenStream2> {
             fn from(value: &str) -> Self {
                 match value {
                     #(#str_to_route,)*
-                    _ => unimplemented!(),
+                    #not_found
                 }
             }
         }
--- src/lib.rs
+++ src/lib.rs
@@ -19,4 +19,21 @@ mod tests {
 
         assert_eq!(Route::Root.to_string(), "/")
     }
+
+    #[test]
+    fn not_found_works() {
+        #[derive(Routes, Debug, PartialEq)]
+        enum Route {
+            #[route("/")]
+            Root,
+            #[route("/frontend/404")]
+            NotFound,
+        }
+
+        assert_eq!(Route::NotFound.to_string(), "/frontend/404");
+
+        let path = "/does-not-exist";
+        let route = Route::from(path);
+        assert_eq!(Route::NotFound, route)
+    }
 }
